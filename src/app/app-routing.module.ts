f614a2dd960a9a6fa3f5866bed8d6d35211ef1d1
Luis VR <luis_vazquez.r@hotmail.com>
2021-02-23 18:03:24 -0600

    animatins modules init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { PortraitComponent } from './pages/portrait/portrait.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 const routes: Routes = [
-  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+  // { path: '**', pathMatch: 'full', redirectTo: 'home'},
   { 
     path: '', 
     component: PortraitComponent, 
@@ -11,6 +14,20 @@ const routes: Routes = [
   { 
     path: 'home', 
     component: PortraitComponent, 
+  },
+  { 
+    path: 'about', 
+    component: AboutComponent, 
+    data: { animation: 'isAbout'}
+  },
+  { 
+    path: 'projects', 
+    component: ProjectsComponent, 
+    data: { animation: 'isProjects'}
+  },
+  { 
+    path: 'contact', 
+    component: ContactComponent, 
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortraitComponent } from './pages/portrait/portrait.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortraitComponent
+    PortraitComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
